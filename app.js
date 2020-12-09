b5d2c0ee79b9d2d6dcce61558b221531816c3a49
Huy Pham Quoc <phamquochuy@tma.com.vn>
2020-12-09 04:20:10 +0000

    add manual approve before build stage

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("Hello Huy Pham!"));
+app.get("/", (req, res) =>
+  res.send(
+    "Hello Huy Pham!\nFull pipeline test with manual approve on build stage."
+  )
+);
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
